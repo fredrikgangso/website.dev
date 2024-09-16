b77dc65f64d898217eea7e1249972bf080901ae1
fredrikgangso <fredrik@gangso.dev>
2024-09-16 21:00:19 +0200

    init template

---
+++ components/S/SButton.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+
+const props = defineProps<{
+    type: 'button' | 'submit' | 'reset'
+    error?: boolean
+    
+}>()
+</script>
+
+
+<template>
+    <input :type :placeholder="props.type" />
+
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,21 @@
+<script lang="ts" setup>
+console.log('Hello, World!')
+</script>
+<template>
+    <div class="bg-slate-200 grow flex p-2 justify-between gap-2">
+        <div class="bg-red-900 grow max-w-sm rounded-md p-2"> 
+            <img src="~/assets/images/lorem-lorem.svg" alt="Nuxt Logo" class="logo" />
+        </div>
+        <div class="bg-red-900 grow">
+            <div class="bg-blue-900 grow flex m-2 gap-2 justify-between">
+                <div class="bg-red-900 grow">s</div>
+                <div class="bg-red-900 grow">s</div>
+                <div class="bg-red-900 grow">s</div>
+
+            </div>
+        </div>
+        <div class="bg-red-900 grow">s</div>
+
+    </div>
+    <NuxtPage />
+</template>
