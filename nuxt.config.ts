cee884a033e7ff123f0b352642fd1a369d6e50c6
fredrikgangso <fredrik@gangso.dev>
2024-09-16 19:52:15 +0200

    add tailwind

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2024-04-03',
-  devtools: { enabled: true }
-})
+  compatibilityDate: "2024-04-03",
+
+  modules: ["@nuxtjs/tailwindcss"],
+  css: ["~/assets/styles.css"],
+  postcss: {
+    plugins: {
+      tailwindcss: {},
+      autoprefixer: {},
+    },
+  },
+  devtools: { enabled: false },
+});
